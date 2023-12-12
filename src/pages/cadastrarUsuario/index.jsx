187420761ee739309745e12343923d1bf98aca89
MatheusAprigio11 <aprigiomatheus24@gmail.com>
2023-12-12 11:32:27 -0300

    updt

--- src/pages/cadastrarUsuario/index.jsx
+++ src/pages/cadastrarUsuario/index.jsx
@@ -1,39 +1,158 @@
-import { View, Text, TextInput } from 'react-native'
+import { View, Text, TextInput, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { useState } from 'react';
 import styles from './styles';
 
 const CadastrarUsuario = () => {
-    const [textCPF, setTextCPF] = useState('Insira aqui seu CPF');
+  const [textCPF, setTextCPF] = useState('Insira aqui seu CPF');
+  const [textFN, setTextFN] = useState('Primeiro Nome');
+  const [textLN, setTextLN] = useState('Sobrenome');
+  const [textNascimento, setTextNascimento] = useState('Data de Nascimento(YYYY-MM-DD)');
+  const [textUsername, setTextUsername] = useState('Usuário');
+  const [textPassword, setTextPassword] = useState('Senha');
+  const [textTel, setTextTel] = useState('Telefone');
 
-    const handleFocusCPF = () => {
-        if (textCPF === 'Insira aqui seu CPF') {
-          setTextCPF('');
-        }
-      };
-    
-      const handleBlurCPF = () => {
-        if (textCPF === '') {
-          setTextCPF('Insira aqui seu CPF');
-        }
-      };
+  const handleFocusCPF = () => {
+    if (textCPF === 'Insira aqui seu CPF') {
+      setTextCPF('');
+    }
+  };
 
-    return (
-        <View>
-            <View>
-                <Text> Cadastrar Usuario </Text>
-            </View>
-            <View>
-                <TextInput
-                        style={styles.input}
-                        onFocus={handleFocusCPF}
-                        onBlur={handleBlurCPF}
-                        onChangeText={(text)  => setTextCPF(text)}
-                        value={textCPF}
-                    />
-            </View>
-        </View>
-    )
+  const handleBlurCPF = () => {
+    if (textCPF === '') {
+      setTextCPF('Insira aqui seu CPF');
+    }
+  };
+  const handleFocusFirstName = () => {
+    if (textFN === 'Primeiro Nome') {
+      setTextFN('');
+    }
+  };
+
+  const handleBlurFirstName = () => {
+    if (textFN === '') {
+      setTextFN('Primeiro Nome');
+    }
+  };
+  const handleFocusLastName = () => {
+    if (textLN === 'Sobrenome') {
+      setTextLN('');
+    }
+  };
+
+  const handleBlurLastName = () => {
+    if (textLN === '') {
+      setTextLN('Sobrenome');
+    }
+  };
+  const handleFocusNascimento = () => {
+    if (textNascimento === 'Data de Nascimento(YYYY-MM-DD)') {
+      setTextNascimento('');
+    }
+  };
+
+  const handleBlurNascimento = () => {
+    if (textNascimento === '') {
+      setTextNascimento('Data de Nascimento(YYYY-MM-DD)');
+    }
+  };
+  const handleFocusUsername = () => {
+    if (textUsername === 'Usuário') {
+      setTextUsername('');
+    }
+  };
+
+  const handleBlurUsername = () => {
+    if (textUsername === '') {
+      setTextUsername('Usuário');
+    }
+  };
+  const handleFocusPassword = () => {
+    if (textPassword === 'Senha') {
+      setTextPassword('');
+    }
+  };
+
+  const handleBlurPassword = () => {
+    if (textPassword === '') {
+      setTextPassword('Senha');
+    }
+  };
+  const handleFocusTel = () => {
+    if (textTel === 'Telefone') {
+      setTextTel('');
+    }
+  };
+
+  const handleBlurTel = () => {
+    if (textTel === '') {
+      setTextTel('Telefone');
+    }
+  };
+
+  return (
+    <View>
+      <View style={styles.titleCadastrar}>
+        <Text style={styles.textCadastrar}> Cadastrar Usuario </Text>
+      </View>
+      <View>
+        <TextInput
+          style={styles.input}
+          onFocus={handleFocusFirstName}
+          onBlur={handleBlurFirstName}
+          onChangeText={(text) => setTextFN(text)}
+          value={textFN}
+        />
+        <TextInput
+          style={styles.input}
+          onFocus={handleFocusLastName}
+          onBlur={handleBlurLastName}
+          onChangeText={(text) => setTextLN(text)}
+          value={textLN}
+        />
+        <TextInput
+          style={styles.input}
+          onFocus={handleFocusCPF}
+          onBlur={handleBlurCPF}
+          onChangeText={(text) => setTextCPF(text)}
+          value={textCPF}
+        />
+        <TextInput
+          style={styles.input}
+          onFocus={handleFocusNascimento}
+          onBlur={handleBlurNascimento}
+          onChangeText={(text) => setTextNascimento(text)}
+          value={textNascimento}
+        />
+        <TextInput
+          style={styles.input}
+          onFocus={handleFocusUsername}
+          onBlur={handleBlurUsername}
+          onChangeText={(text) => setTextUsername(text)}
+          value={textUsername}
+        />
+        <TextInput
+          style={styles.input}
+          onFocus={handleFocusPassword}
+          onBlur={handleBlurPassword}
+          onChangeText={(text) => setTextPassword(text)}
+          value={textPassword}
+        />
+        <TextInput
+          style={styles.input}
+          onFocus={handleFocusTel}
+          onBlur={handleBlurTel}
+          onChangeText={(text) => setTextTel(text)}
+          value={textTel}
+        />
+      </View>
+      <View style={styles.btnView}>
+          <TouchableOpacity style={styles.btnOk}>
+            <Text style={{ fontFamily: 'semibold', fontSize: 20 }}>Ok</Text>
+          </TouchableOpacity>
+      </View>
+    </View>
+  )
 }
 
 export default CadastrarUsuario
--- src/pages/cadastrarUsuario/styles.jsx
+++ src/pages/cadastrarUsuario/styles.jsx
@@ -3,11 +3,46 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
     input:{
         backgroundColor: '#FDBCBC',
-        borderRadius: 12,
+        borderRadius: 8,
         height: 40,
+        width: 350,
         fontFamily: 'regular',
-        paddingLeft: 8
+        paddingLeft: 8,
+        marginLeft: 10,
+        marginTop: 10,
+        
     },
+
+    textCadastrar:{
+        color: '#ff364e',
+        fontFamily: 'semibold',
+        fontSize: 30,
+    },
+
+    titleCadastrar:{
+        justifyContent: "center",
+        alignItems: 'center',
+        marginTop: 100,
+        marginBottom: 50
+    },
+
+    btnOk:{
+        justifyContent: "center",
+        borderRadius: 13,
+        alignItems: "center",
+        backgroundColor: '#ff364e',
+        width: 140,
+        height: 40,
+        marginTop: 15,
+        marginBottom:40,
+        
+    },
+
+    btnView:{
+        marginTop: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
 })
 
 export default styles
